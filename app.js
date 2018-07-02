b8835e78f1b749635f0be09572da982be19e6ba9
Ajay Thakran <ajaythakran1998@gmail.com>
2018-07-02 16:36:57 +0530

    handled auth

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
-const port = process.env.PORT || 5000 ;
-
 app.get('/', (req, res) => {
-	res.send('works');
-})
+  res.send('It Works!');
+});
+
+// Use Routes
+app.use('/auth', auth);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
+  console.log('Connected to server');
 });
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI : 'mongodb://Fire Fist:A19041998@@ds125021.mlab.com:25021/nuts',
+	googleClientID : '617873533214-1a103veuevs9moa0gt4ruk6iuvrtaith.apps.googleusercontent.com',
+	googleClientSecret : 'FRowOs-obhX3GstDoLEkEkyb'
+}
