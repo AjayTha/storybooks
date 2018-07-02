08daf9c975d7d17ac2bc975b6f6d3268b34d28fc
Ajay Thakran <ajaythakran1998@gmail.com>
2018-07-02 18:00:13 +0530

    handled auth

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	mongoURI : 'mongodb://Fire Fist:A19041998@@ds125021.mlab.com:25021/nuts',
-	googleClientID : '617873533214-1a103veuevs9moa0gt4ruk6iuvrtaith.apps.googleusercontent.com',
-	googleClientSecret : 'FRowOs-obhX3GstDoLEkEkyb'
-}
